a6abe6144ceed8569cb0f590ffcfb96f4afc44ad
paul <paul.mount@gmail.com>
2020-11-23 14:55:26 -0500

    Initial commit

---
+++ src/test/java/sandbox/paul/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sandbox.paul.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
